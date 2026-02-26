bd53adeadfecbe49681a991911c1e158825ce590
Shreyas8905 <shreyaskulkarni75@outlook.com>
2026-02-26 19:53:42 +0530

    Added feature extraction and clustering code

---
+++ general_test/clustering.py
@@ -0,0 +1,12 @@
+import umap
+import hdbscan
+
+def cluster(latent_features):
+    print("Mapping Latent space to 2d with UMAP...")
+    reducer = umap.UMAP(n_neighbors=15, min_dist=0.0, n_components=2, random_state=42)
+    embedding = reducer.fit_transform(latent_features)
+    print("Identifying clusters with HDBSCAN...")
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=5, gen_min_span_tree=True)
+    cluster_labels = clusterer.fit_predict(embedding)
+
+    return embedding, cluster_labels
---
+++ general_test/feature_extraction.py
@@ -0,0 +1,28 @@
+import torch
+import glob
+import numpy as np
+from torch.utils.data import DataLoader
+from data_preprocessing import ALMADataset, transform
+
+def feature_extraction():
+    print("Extracting structural fingerprints...")
+    
+    model = 'autoencoder.pth'
+    device = torch.device("cpu")
+    data_dir = "./data/*.fits"
+    file_paths = glob.glob(data_dir)
+    dataset = ALMADataset(file_paths, transforms=transform)
+    dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+    model.eval()
+    latent_features = []
+    image_paths = []
+
+    with torch.no_grad():
+        for data, paths in dataloader:
+            data = data.to(device)
+            latent, _ = model(data)
+            latent_features.extend(latent.cpu().numpy())
+            image_paths.extend(paths)
+    latent_features = np.array(latent_features)
+
+    return latent_features
